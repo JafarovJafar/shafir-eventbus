ef5c444e0e25f9c5af219b9a380e9bedd3ad25e2
jafarov ja <jafarovja@yandex.ru>
2024-11-21 00:14:44 +0700

    add all common methods

--- ShafirEventBus.cs
+++ ShafirEventBus.cs
@@ -1,18 +1,42 @@
 using System;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class ShafirEventBus
 {
-    private Dictionary<Type, List<Action>> _actions;
+    private Dictionary<Type, Delegate> _delegates;
 
-    public void Subscribe<T>()
+    public ShafirEventBus()
     {
+        _delegates = new Dictionary<Type, Delegate>();
+    }
 
+    public void Subscribe<T>(Action<T> action)
+    {
+        if (!_delegates.ContainsKey(typeof(T)))
+        {
+            _delegates.Add(typeof(T), null);
+        }
+
+        var existingDelegate = _delegates[typeof(T)];
+        existingDelegate = Delegate.Combine(existingDelegate, action);
+        _delegates[typeof(T)] = existingDelegate;
+    }
+
+    public void UnSubscribe<T>(Action<T> action)
+    {
+        if (!_delegates.ContainsKey(typeof(T))) return;
+
+        var existingDelegate = _delegates[typeof(T)];
+        existingDelegate = Delegate.Remove(existingDelegate, action);
+        _delegates[typeof(T)] = existingDelegate;
     }
 
     public void Publish<T>(T message)
     {
+        if (!_delegates.ContainsKey(typeof(T))) return;
 
+        var existingDelegate = _delegates[typeof(T)];
+        var action = (Action<T>)existingDelegate;
+        action(message);
     }
 }

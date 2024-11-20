4f6970c510b975ae0a3a020fd3a722229187a359
jafarov ja <jafarovja@yandex.ru>
2024-11-20 23:37:31 +0700

    wip

---
+++ ShafirEventBus.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShafirEventBus
+{
+    private Dictionary<Type, List<Action>> _actions;
+
+    public void Subscribe<T>()
+    {
+
+    }
+
+    public void Publish<T>(T message)
+    {
+
+    }
+}
